a01eefe2d819b92be32c2423113841c98e81622b
KingSimpa69 <kingsimpa69@gmail.com>
2023-10-17 06:54:05 -0600

    Added dynamic route capabilities

--- pages/[...collection].js
+++ pages/[...collection].js
@@ -1,9 +1,10 @@
 import { useEffect, useState } from 'react';
 import dynamic from "next/dynamic";
-import styles from '../styles/collection.module.css';
+import styles from '@/styles/collection.module.css';
 import Filter from '@/components/collection/Filter';
 import HorizontalRule from '@/components/HorizontalRule';
 import FellaModal from '@/components/collection/FellaModal';
+import { useRouter } from 'next/router';
 
 const Loader = dynamic(
   () => import('@/components/collection/Loader'),
@@ -23,6 +24,25 @@ export default function Collection() {
   const [necklace,setNecklace] = useState("")
   const [outfit,setOutfit] = useState("")
   const [type,setType] = useState("")
+
+  const router = useRouter()
+
+  const getMeta = async (id) => {
+    const response = await fetch(`/api/nft?from=1&to=2&id=${id}`)
+    const data = await response.json();
+    setActiveMeta(await data.nfts[0].attributes)
+  }
+
+  useEffect(() => {
+    if(router.query.collection){
+      if(router.query.collection[1] !== undefined){
+        getMeta(router.query.collection[1])
+        setActive(parseInt(router.query.collection[1]))
+        modalOpen(true)
+      }
+    }
+  }, [router])
+  
 
   const toggleModal = (id) => {
     setActive(id)
